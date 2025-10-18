37bf283c7812ca11dfd77dcb34bf39059947602d
nikil58 <titata1@mail.ru>
2025-10-18 18:38:19 +0300

    feat: add Aivor

--- source/quartz/components/Graph.tsx
+++ source/quartz/components/Graph.tsx
@@ -51,7 +51,7 @@ const defaultOptions: GraphOptions = {
     fontSize: 0.6,
     opacityScale: 1,
     showTags: true,
-    removeTags: [],
+    removeTags: ["ignore"],
     focusOnHover: true,
   },
 }
